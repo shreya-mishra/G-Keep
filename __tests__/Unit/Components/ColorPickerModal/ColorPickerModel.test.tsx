f7ad4a507dab805d5afe17f0257d6f43be7d73db
shreya-mishra <shreyamishra040398@gmail.com>
2023-12-07 21:06:55 +0530

    added testing

---
+++ __tests__/Unit/Components/ColorPickerModal/ColorPickerModel.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ColorPickerModal from "../../../../src/module/index/components/ColorPickerModal";
+import { render } from "@testing-library/react-native";
+import "@testing-library/jest-native/extend-expect";
+
+describe("ColorPickerModel", () => {
+  test("should render ColorPickerModel", () => {
+    const { getByTestId } = render(
+      <ColorPickerModal
+        testId="colorPickerModel"
+        visible={false}
+        onClose={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        onSelectColor={function (color: string): {} {
+          throw new Error("Function not implemented.");
+        }}
+      />
+    );
+    const colorPickerModel = getByTestId("colorPickerModel");
+    expect(colorPickerModel).toBeInTheDocument();
+  });
+});
